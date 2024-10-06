1a8b72fb1b3d59d35d3895517b5d9d02e1dc12dc
samharris0509 <samhancock2023@gmail.com>
2024-10-06 11:02:45 -0500

    issues

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -1,32 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MovieList.Models;
-using System.Diagnostics;
 
 namespace MovieList.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private MovieContext context { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(MovieContext ctx)
         {
-            _logger = logger;
+            context = ctx;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var movies = context.Movies.Include(m => m.Genre)
+                .OrderBy(m => m.Name).ToList();
+            return View(movies);
         }
     }
 }
