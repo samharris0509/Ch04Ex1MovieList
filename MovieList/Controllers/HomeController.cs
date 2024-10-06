14022c7c0184a192f21d7531f514ee8573b0c2e2
samharris0509 <samhancock2023@gmail.com>
2024-10-06 12:38:44 -0500

    final

--- MovieList/Controllers/HomeController.cs
+++ MovieList/Controllers/HomeController.cs
@@ -7,16 +7,17 @@ namespace MovieList.Controllers
 {
     public class HomeController : Controller
     {
-        private MovieContext context { get; set;  }
+        private MovieContext context { get; set; }
 
         public HomeController(MovieContext ctx)
         {
             context = ctx;
         }
+
         public IActionResult Index()
         {
-            var movies = context.Movies.OrderBy(m => m.Name).ToList();
-           
+            var movies = context.Movies.Include(m => m.Genre)
+                .OrderBy(m => m.Name).ToList();
             return View(movies);
         }
     }
